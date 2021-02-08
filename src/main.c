7a468baacedbbb15dfbc686a8c4138b081c4b337
rcarmen <rcarmen@student.21-school.ru>
2021-02-08 13:01:40 +0300

    Reload

--- src/main.c
+++ src/main.c
@@ -4,9 +4,13 @@ int main()
 {
 	void	*mlx;
 	//void    *mlx_win;
+	void 	*img;
 
 	mlx = mlx_init();
 	//mlx_win = mlx_new_window(mlx, 512, 512, "Hello world!");
-	mlx_new_window(mlx, 512, 512, "Hello");
-	mlx_loop(mlx);
+	//mlx_new_window(mlx, 512, 512, "Hello");
+	//img = mlx_new_image(mlx, 512, 512);
+	mlx_new_image(mlx, 512, 512);
+
+	//mlx_loop(mlx);
 }
