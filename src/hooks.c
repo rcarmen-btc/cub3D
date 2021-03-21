13f607844c2c07cbc0c10c851049ccc3a584c7ac
rcarmen <rcarmen@student.21-school.ru>
2021-03-21 21:35:08 +0300

    Refactoring

--- src/hooks.c
+++ src/hooks.c
@@ -91,13 +91,13 @@ void					smooth_move(t_set *set)
 	set->pattr.playeryd = set->tabs.fsint[set->pattr.fpa];
 	if (set->kfl.left == 1)
 	{
-		if ((set->pattr.fpa -= set->ray.angle10 / (15 - set->pattr.fpseed)) < set->ray.angle0)
-			set->pattr.fpa += set->ray.angle360;
+		if ((set->pattr.fpa += set->ray.angle10 / (15 - set->pattr.fpseed)) < set->ray.angle0)
+			set->pattr.fpa -= set->ray.angle360;
 	}
 	if (set->kfl.right == 1)
 	{
-		if ((set->pattr.fpa += set->ray.angle10 / (15 - set->pattr.fpseed)) >= set->ray.angle360)
-			set->pattr.fpa -= set->ray.angle360;
+		if ((set->pattr.fpa -= set->ray.angle10 / (15 - set->pattr.fpseed)) >= set->ray.angle360)
+			set->pattr.fpa += set->ray.angle360;
 	}
 	set->pattr.playerxd = set->tabs.fcost[set->pattr.fpa];
 	set->pattr.playeryd = set->tabs.fsint[set->pattr.fpa];
